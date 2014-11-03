09eadac0cb328ba1f2f1e5aa27aa028280072e83
Jason Hite <jasonmhite@gmail.com>
2014-11-02 19:14:47 -0500

    alskdfjkl;as

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ocdsort',
     version='0.0.2',
-    packages=['ocdsort'],
+    packages=['ocdsort', 'ocdsort.cli'],
     install_requires=[
         'click',
         'guessit',
