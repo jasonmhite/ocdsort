d3c83748a674ecd2f9e4c1ba119194919ba7820a
Jason Hite <jasonmhite@gmail.com>
2014-11-08 14:35:43 -0500

    More complex handling for indent

--- ocdsort/util/indent.py
+++ ocdsort/util/indent.py
@@ -3,26 +3,55 @@ from click import wrap_text
 ## The CLI will import this into builtins, so it has to be imported on
 # its own
 
+class IndentState(object):
+    def __init__(self):
+        self._state = []
+
+    def __iadd__(self, other):
+        self._state.append(other)
+
+    @property
+    def as_string(self):
+        return("".join(self._state))
+
+    @property
+    def extra_indent(self)
+        return(self.as_string + self._state[-1])
+
+    @property
+    def level(self):
+        return(len(self._state))
+
+    def __str__(self):
+        return(self.as_string)
+
+    def pop(self):
+        self._state.pop()
+
+
 class Indent(object):
+
     def __init__(self, indent_char="  ", initial_indent=0, max_width=78):
-        self._ilevel = initial_indent
         self._indent_char = indent_char
         self._max_width = max_width
 
+        self._indent_string = IndentState()
+        for i in range(initial_indent):
+            self._indent_string += self._indent_char
+
     def __call__(self, s):
-        indent = self._ilevel * self._indent_char
-        indent2 = indent + self._indent_char
         new_s = wrap_text(
             s,
             width=self._max_width,
-            initial_indent=indent,
-            subsequent_indent=indent2,
+            initial_indent=self._indent_string.as_string,
+            subsequent_indent=self._indent_string.extra_indent,
         )
         return(new_s)
 
-    def __enter__(self):
-        self._ilevel += 1
+    def __enter__(self, local_indent=None):
+        i = (indent_char if local_indent is None else local_indent)
+        self._indent_string += i
         return(self)
 
     def __exit__(self, type, value, tb):
-        self._ilevel -= 1
+        self._indent_string.pop()
